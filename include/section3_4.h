a3142d606e4b625c5ad814bc7d4a2c4440ec0eeb
TCI03 <cimino.thomas@gmail.com>
2024-11-27 16:09:57 +0100

    Add section3_4 and 3_5 headers.

---
+++ include/section3_4.h
@@ -0,0 +1,8 @@
+#ifndef SECTION3_4
+#define SECTION3_3
+
+int countdwso(void);
+void escapekr(char [], char []);
+void unescapekr(char [], char []);
+
+#endif
---
+++ include/section3_5.h
@@ -0,0 +1,9 @@
+#ifndef SECTION3_5
+#define SECTION3_5
+
+int atoi2(char []);
+void shellsortkr(int [], int);
+void reversekr(char []);
+void expandkr(char [], char []);
+
+#endif
