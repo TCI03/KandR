f963186c41d7e78166470ea26050d339f1d84018
TCI03 <cimino.thomas@gmail.com>
2024-11-27 17:32:45 +0100

    Add section3_6 and 3_7 source code.

---
+++ src/section3_6.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+#include "section3_5.h"
+#include "section3_6.h"
+
+#define abs(x) ((x) < 0 ? -(x) : (x))
+
+void itoa(int n, char s[])
+{
+    int i, sign;
+
+    if ((sign = n) < 0) {
+        n = -n;
+    }
+    i = 0;
+    do {
+        s[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reversekr(s);
+}
+
+/* Exercice 3-4 */
+void itoa2(int n, char s[])
+{
+    int i, sign;
+
+    sign = n;
+    i = 0;
+    do {
+        s[i++] = abs(n % 10) + '0';
+    } while ((n /= 10) > 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reversekr(s);
+}
+
+/* Exercise 3-5 */
+void itob(int n, char s[], int b)
+{
+    int i, j, sign;
+
+    if ((sign = n) < 0) {
+        n = -n;
+    }
+
+    i = 0;
+    do {
+        j = n % b;
+        s[i++] = (j <= 9) ? j+'0' : j+'a'-10;
+    } while ((n /= b) > 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reversekr(s);
+}
+
+/* Exercice 3-6 */
+void itoa3(int n, char s[], int w)
+{
+    int i, sign;
+
+    sign = n;
+    i = 0;
+    do {
+        s[i++] = abs(n % 10) + '0';
+    } while ((n /= 10) != 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+
+    while (i < w) {
+        s[i++] = ' ';
+    }
+    s[i] = '\0';
+    reversekr(s);
+}
---
+++ src/section3_7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "section3_7.h"
+
+int trim(char s[])
+{
+    int n;
+
+    for (n = strlen(s) - 1; n >= 0; n--) {
+        if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n') {
+            break;
+        }
+    }
+
+    s[n+1] = '\0';
+
+    return n;
+}
